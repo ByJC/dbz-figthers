b341ebf97bf3e0fea7ec3425ea3da6ebb2ddeddc
ByJC <jeancharles.fauvin@gmail.com>
2018-02-18 14:37:04 +0100

    [FEATURE] Add delete battle when in progress

--- src/app/battles/battle/battle.component.ts
+++ src/app/battles/battle/battle.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { FirebaseService } from '../../shared/firebase.service';
+import { ProgressBarService } from '../../progressbar/progressbar.service';
 
 @Component({
   selector: 'battle-item',
@@ -8,22 +9,37 @@ import { FirebaseService } from '../../shared/firebase.service';
 })
 export class BattleComponent {
   @Input() battle: any;
-  constructor(private fb: FirebaseService) { }
+  constructor(private fb: FirebaseService, private $progressbar: ProgressBarService) { }
 
   selectWinner(battle, player) {
-
+    this.$progressbar.show();
     const dataToBeUpdated = {
       "isFinish": true,
       "firstPlayer.winner": (player === "firstPlayer"),
       "secondPlayer.winner": (player === "secondPlayer")
     };
-
+    
     this.fb.updateBattle(battle, dataToBeUpdated)
       .then(() => {
+        this.$progressbar.hide();
         console.log("Document successfully updated!");
       })
       .catch(error => {
+        this.$progressbar.hide();
         console.error("Error updating document: ", error);
       });
+  }
+
+  deleteBattle(battle) {
+    this.$progressbar.show();
+    this.fb.deleteBattle(battle)
+    .then(() => {
+      this.$progressbar.hide();
+    })
+    .catch(error => {
+      this.$progressbar.hide();
+      console.error("Error updating document: ", error);
+    });
+
   }
 }
